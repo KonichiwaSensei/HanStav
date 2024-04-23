46aba89f51b8cb0423f0e2eca1f47ae659fe35ea
Matthew Shenar <matthew.shenar@gmail.com>
2024-04-23 16:10:49 +0200

    added react-helmet for gtag.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useEffect } from "react"
-import { Route, Routes, useLocation } from "react-router-dom"
+import { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 
 import './App.scss'
-import NavigationBar from "./Components/NavigationBar"
-import Footer from "./Components/Footer"
+import NavigationBar from './Components/NavigationBar'
+import Footer from './Components/Footer'
 import Home from './Pages/Home'
-import Reference from "./Pages/Reference"
-import Stavby from "./Pages/Stavby"
-import RodDom from "./Pages/RodDom"
-import InzCin from "./Pages/InzCin"
-import ProjKan from "./Pages/ProjKan"
-import Hypoteka from "./Pages/Hypoteka"
-import Reality from "./Pages/Reality"
-import Kontakt from "./Pages/Kontakt"
+import Reference from './Pages/Reference'
+import Stavby from './Pages/Stavby'
+import RodDom from './Pages/RodDom'
+import InzCin from './Pages/InzCin'
+import ProjKan from './Pages/ProjKan'
+import Hypoteka from './Pages/Hypoteka'
+import Reality from './Pages/Reality'
+import Kontakt from './Pages/Kontakt'
 
 function App() {
 
@@ -24,7 +25,19 @@ function App() {
 
   return (
     <>
-      {/* <BrowserRouter> */}
+      <Helmet>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-KSSSKJHJSS"></script>
+      <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-KSSSKJHJSS');
+          `}
+        </script>
+      </Helmet>
 
         <NavigationBar />
 
@@ -35,14 +48,14 @@ function App() {
           <Route path="/projekty-rodinnych-domu" element={<RodDom />} />
           <Route path="/inzenyrska-cinnost" element={<InzCin />} />
           <Route path="/projekcni-kancelar" element={<ProjKan />} />
-          {/* <Route path="/hypoteka" element={<Hypoteka />} /> */}
+          {/* <Route path="/hypoteka" element={<Hypoteka />} />  */}
           <Route path="/reality" element={<Reality />} />
           <Route path="/kontakty" element={<Kontakt />} />
         </Routes>
 
         <Footer />
 
-      {/* </BrowserRouter> */}
+      
     </>
   )
 }
